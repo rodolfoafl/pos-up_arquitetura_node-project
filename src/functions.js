bc1eb6977915e4219d5d8763d220a5445f1221d2
Rodolfo <27327749+rodolfoafl@users.noreply.github.com>
2021-07-14 14:55:43 -0300

    Implemented player score logic

--- src/functions.js
+++ src/functions.js
@@ -1,3 +1,18 @@
+//#region Variáveis e Métodos para manipulação da pontuação
+//Pontuação do jogador
+let playerScore = 0;
+
+//Reseta a pontuação do jogador
+const resetPlayerScore = () => {
+  playerScore = 0;
+};
+
+//Atualiza a pontuação do jogador
+const updatePlayerScore = () => {
+  playerScore++;
+};
+//#endregion
+
 export const startQuiz = () => {
   console.log("Starting quiz...");
 };
